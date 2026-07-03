53ac2dded7d5ff22ba080c66689f288b5cd452fe
Daniel C <brikbusters@gmail.com>
2026-07-03 19:09:13 -0400

    update gradle plugin and gradle version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
     defaultConfig {
         applicationId = "dev.danielc"
         minSdk = 24
-        targetSdk = 36
+        targetSdk = 37
         versionCode = 1
         versionName = "1.0"
 
